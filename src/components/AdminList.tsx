e70bfbc4a70c45452384519069c91a76351ab6ca
Ichiroki <fahrezirizqiawan12649@gmail.com>
2024-01-21 22:37:18 +0700

    project init

---
+++ src/components/AdminList.tsx
@@ -0,0 +1,23 @@
+type AdminListType = {
+    title: string
+    admin: string[]
+}
+
+function AdminList({title, admin}: AdminListType) {
+    return (
+        <>
+            <div className="mb-12 flex flex-col items-center">
+                <h1 className="text-xl text-center mb-6 font-semibold border-b-2 w-fit">{title}</h1>
+                <ul className="flex flex-col items-center justify-center gap-10">
+                    {
+                        admin.map((a) => (
+                            <li>{a}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default AdminList;
