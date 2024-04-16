76ad4778fde85cd3849ebd71d36a548c003c622f
Ichiroki <fahrezirizqiawan12649@gmail.com>
2024-04-16 09:31:35 +0700

    update 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/swdm/',
+  base: '/swdm',
   build: {
     assetsDir: './public'
   },
